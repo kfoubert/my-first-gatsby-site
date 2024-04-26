116ee65badc524c534288d20776af6be126ce492
Ken Foubert <kfoubert@sitecrafting.com>
2024-04-26 16:36:04 -0700

    Finished Gatsby Tutorial Part 6

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,22 +1,26 @@
 import * as React from 'react'
-import { graphql } from 'gatsby'
-import Layout from '../components/layout'
-import Seo from '../components/seo'
+import { Link, graphql } from 'gatsby'
+import Layout from '../../components/layout'
+import Seo from '../../components/seo'
 
 const BlogPage = ({ data }) => {
-    return (
-        <Layout pageTitle="My Blog Posts">
-            {
-                data.allMdx.nodes.map((node) => (
-                    <article key={node.id}>
-                        <h2>{node.frontmatter.title}</h2>
-                        <p>Posted: {node.frontmatter.date}</p>
-                        <p>{node.excerpt}</p>
-                    </article>
-                ))
-            }
-        </Layout>
-    )
+  return (
+    <Layout pageTitle="My Blog Posts">
+      {
+        data.allMdx.nodes.map((node) => (
+          <article key={node.id}>
+            <h2>
+              <Link to={`/blog/${node.frontmatter.slug}`}>
+                {node.frontmatter.title}
+              </Link>
+            </h2>
+            <p>Posted: {node.frontmatter.date}</p>
+            <p>{node.excerpt}</p>
+          </article>
+        ))
+      }
+    </Layout>
+  )
 }
 
 // The query is a GraphQL query that fetches the names of all files in the project.
@@ -28,6 +32,7 @@ query {
         frontmatter {
           date(formatString: "MMMM D, YYYY")
           title
+          slug
         }
         id
         excerpt

f3e9cc7d7efddd7085576d815de4e6e9cc8a8aae
Ken Foubert <kfoubert@sitecrafting.com>
2024-04-26 16:11:16 -0700

    Finished Gatsby Tutorial Part 5

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -6,15 +6,15 @@ import Seo from '../components/seo'
 const BlogPage = ({ data }) => {
     return (
         <Layout pageTitle="My Blog Posts">
-            <ul>
-                {
-                    data.allFile.nodes.map(node => (
-                        <li key={node.name}>
-                            {node.name}
-                        </li>
-                    ))
-                }
-            </ul>
+            {
+                data.allMdx.nodes.map((node) => (
+                    <article key={node.id}>
+                        <h2>{node.frontmatter.title}</h2>
+                        <p>Posted: {node.frontmatter.date}</p>
+                        <p>{node.excerpt}</p>
+                    </article>
+                ))
+            }
         </Layout>
     )
 }
@@ -22,10 +22,15 @@ const BlogPage = ({ data }) => {
 // The query is a GraphQL query that fetches the names of all files in the project.
 // Gatbsy will run this query and pass the result to the BlogPage component as a prop called data.
 export const query = graphql`
-  query {
-    allFile {
+query {
+    allMdx(sort: {frontmatter: {date: DESC}}) {
       nodes {
-        name
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
+        }
+        id
+        excerpt
       }
     }
   }
